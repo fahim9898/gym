b6b71d2ab3877a846efd91cd529d47b9025fe5c4
Fahim Sanghariyat <sanghariyatfahim99@gmail.com>
2022-01-27 19:04:05 +0530

    deploy

--- assets/Dashboard.5cb180d4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./vendor.133fbbfd.js";const s=()=>a("div",{className:"h4",children:"DashBoard"});export{s as default};
---
+++ assets/Dashboard.bf1aff3e.js
@@ -0,0 +1,1 @@
+import{a}from"./vendor.76084820.js";const s=()=>a("div",{className:"h4",children:"DashBoard"});export{s as default};
